4a666287bcdfbbaf817997dfa281b74b28cd516e
“Sam <sam.alsmadi@gmail.com>
2018-03-30 01:30:41 -0400

    Lazy load new-post route for performance improvement

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,21 @@ import React, { Component } from 'react';
 // import Post from '../../components/Post/Post';
 // import FullPost from '../../components/FullPost/FullPost';
 // import NewPost from '../../components/NewPost/NewPost';
+// import FullPost from './FullPost/FullPost';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
-// import FullPost from './FullPost/FullPost';
+
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     render () {
 
@@ -45,7 +51,8 @@ class Blog extends Component {
                     <NewPost />
                 </section> */}
                 <Switch>
-                    {this.state.auth? <Route path="/new-post" component={NewPost} /> : null}
+                    {/* {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null} */}
+                    {this.state.auth? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     {/* <Route path="/" component={Posts} /> */}
                     {/* <Redirect from="/" to="/posts" /> */}
